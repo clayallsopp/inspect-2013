78d82c26304459fa124c86403e26930a90a6d9aa
Clay Allsopp <clay.allsopp@gmail.com>
2013-03-29 01:30:42 +0100

    Create constants.rb

---
+++ constants.rb
@@ -0,0 +1,17 @@
+module Constants
+  module_function
+
+  def const_int_get(base, value)
+    case value
+    when Integer
+      value
+    when NSArray
+      value.reduce { |i, j|
+        const_int_get(base, i) | const_int_get(base, j)
+      }
+    else
+      value = value.to_s.camelize
+      Kernel.const_get("#{base}#{value}")
+    end
+  end
+end
